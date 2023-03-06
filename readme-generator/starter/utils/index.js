c32bfc82a3a81164bb350aea37bd962a40fafcfb
Sylar <aartimehar@hotmail.com>
2023-03-06 11:13:17 +0000

    update

--- readme-generator/starter/utils/index.js
+++ readme-generator/starter/utils/index.js
@@ -1,6 +1,6 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require('path');
+import inquirer from 'inquirer';
 const generateMarkdown = require("./utils/generateMarkdown");
 
 
