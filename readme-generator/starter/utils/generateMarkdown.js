74cadcdda9ec1bfb9749f1ad6797650fb7488d3e
Sylar <aartimehar@hotmail.com>
2023-03-06 00:10:45 +0000

    creaded file generator

--- readme-generator/starter/utils/generateMarkdown.js
+++ readme-generator/starter/utils/generateMarkdown.js
@@ -1,7 +1,43 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ![${data.license[0]}](${data.license[1]}) 
+  
+  
+  ## Description
+  ${data.description}
+    
+  ## Table of Contents
+  * [Description](#description)
+  * [Table of Contents](#table-of-contents)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+    
+    
+  ## Installation
+  ${data.installation}
+    
+  ## Usage
+  ${data.usage}
+    
+  ## License
+  Copyright &copy; 2023 ${data.name}       
+  This project is [${data.license[0]}](${data.license[2]}) licensed
+    
+  ## Contributing
+  ${data.contributing}
+    
+  ## Tests
+  ${data.test}
+    
+  ## Questions
+  Feel free to contact me for additional questions and I will get back to you  
+  Email: [${data.email}](#)       
+  Github profile: [${data.github}](https://github.com/${data.github})
 `;
 }
 
