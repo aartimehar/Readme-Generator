cf786518dfc6d10bd9e5fefe0cd78929bb3e1c8b
Sylar <aartimehar@hotmail.com>
2023-03-07 22:10:16 +0000

    update

--- bin/markdownGenerator.js
+++ bin/markdownGenerator.js
@@ -1,6 +1,7 @@
 function generateMarkdown(data) {
     return `# ${data.title}
-    ##### ${data.logo}[${data.userName}](${data.GitHub}) [Email Me](${data.email}) 
+    ##### ${data.logo}
+    [${data.userName}](${data.GitHub}) [Email Me](${data.email}) 
     
     ## User Story
     ### As a developer, I want a README generator so that I can quickly create a professional README for a new project.
@@ -19,8 +20,7 @@ function generateMarkdown(data) {
     * [References] (#References)
     * [License] (#Licence)
 
-    # Repository Description
-   
+## Repository Description
   - When a user enters the project title then it is displayed as the title of the README
   - When a user enters a description, installation instructions, usage information, contribution guidelines, and test instructions then this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
   - When a user chooses a license for their application from a list of options then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
